f0d9b5d0ac06838d4fb192cb147c678005c50f93
Gustavo <gustavo7dias@gmail.com>
2021-02-01 17:11:43 -0300

    Refactor functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,50 +1,4 @@
-let searchGithubUser = document.querySelector('#search-github-user');
-
-let hideAndShowSearch = function () {
-    if (searchGithubUser.style.display !== 'block') {
-        searchGithubUser.style.display = 'block';
-    } else {
-        searchGithubUser.style.display = 'none';
-    }
-}
-
-let searchAndConfirm = document.querySelector('#confirm');
-searchAndConfirm.addEventListener('click', function () {
-    getUserGithub();
-    hideAndShowSearch();
-})
-
-let edit = document.querySelector('#edit');
-edit.addEventListener('click', hideAndShowSearch);
-
-let closeTab = document.querySelector('#close-tab');
-closeTab.addEventListener('click', hideAndShowSearch);
-
-let changeInformations = function (myDatas) {
-    document.querySelector('#profile-avatar').setAttribute('src', myDatas.avatar);
-    document.querySelector('#profile-link').setAttribute('href', myDatas.linkProfile);
-    document.querySelector('#profile-link').innerText = myDatas.name;
-    document.querySelector('#login-name').innerText = myDatas.login;
-    document.querySelector('#counter-01').innerText = myDatas.repos;
-    document.querySelector('#counter-02').innerText = myDatas.followers;
-    document.querySelector('#counter-03').innerText = myDatas.following;
-}
-
-let getDatas = function (data) {
-    let myDatas = {
-        name: data.name,
-        login: data.login,
-        followers: data.followers,
-        repos: data.public_repos,
-        avatar: data.avatar_url,
-        linkProfile: data.html_url,
-        following: data.following
-    };
-    changeInformations(myDatas);
-}
-
-
-let getUserGithub = function () {
+function getUserGithub() {
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function () {
@@ -62,3 +16,48 @@ let getUserGithub = function () {
     xhr.open('GET', endpoint, true);
     xhr.send();
 }
+
+function getDatas(data) {
+    let myDatas = {
+        name: data.name,
+        login: data.login,
+        followers: data.followers,
+        repos: data.public_repos,
+        avatar: data.avatar_url,
+        linkProfile: data.html_url,
+        following: data.following
+    };
+    changeInformations(myDatas);
+}
+
+function changeInformations(myDatas) {
+    document.querySelector('#profile-avatar').setAttribute('src', myDatas.avatar);
+    document.querySelector('#profile-link').setAttribute('href', myDatas.linkProfile);
+    document.querySelector('#profile-link').innerText = myDatas.name;
+    document.querySelector('#login-name').innerText = myDatas.login;
+    document.querySelector('#counter-01').innerText = myDatas.repos;
+    document.querySelector('#counter-02').innerText = myDatas.followers;
+    document.querySelector('#counter-03').innerText = myDatas.following;
+}
+
+let searchGithubUser = document.querySelector('#search-github-user');
+
+function hideAndShowSearch() {
+    if (searchGithubUser.style.display !== 'block') {
+        searchGithubUser.style.display = 'block';
+    } else {
+        searchGithubUser.style.display = 'none';
+    }
+}
+
+let edit = document.querySelector('#edit');
+edit.addEventListener('click', hideAndShowSearch);
+
+let closeTab = document.querySelector('#close-tab');
+closeTab.addEventListener('click', hideAndShowSearch);
+
+let searchAndConfirm = document.querySelector('#confirm');
+searchAndConfirm.addEventListener('click', function () {
+    getUserGithub();
+    hideAndShowSearch();
+})

0da58034ae2f795ec510ce36704051fdaadc233b
Gustavo <gustavo7dias@gmail.com>
2021-02-02 17:17:33 -0300

    Add new functions

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,8 @@
-function getUserGithub() {
+function getUserLogin() {
+    return document.querySelector('#login-name-input').value;
+}
+
+function getGithubUser() {
     let xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function () {
@@ -11,35 +15,74 @@ function getUserGithub() {
         }
     }
 
-    let loginNameInput = document.querySelector('#login-name-input');
-    let endpoint = `https://api.github.com/users/${loginNameInput.value}`;
+    let endpoint = `https://api.github.com/users/${getUserLogin()}`;
     xhr.open('GET', endpoint, true);
     xhr.send();
+
+    function getDatas(data) {
+        let myDatas = {
+            name: data.name,
+            bio: data.bio,
+            login: data.login,
+            followers: data.followers,
+            reposNum: data.public_repos,
+            avatar: data.avatar_url,
+            linkProfile: data.html_url,
+            following: data.following
+        };
+        setInformations(myDatas);
+    }
+
+    function setInformations(myDatas) {
+        document.querySelector('#profile-avatar').setAttribute('src', myDatas.avatar);
+        document.querySelector('#profile-link').setAttribute('href', myDatas.linkProfile);
+        document.querySelector('#profile-link').innerText = myDatas.name;
+        document.querySelector('#login-name').innerText = myDatas.login;
+        document.querySelector('#bio').innerText = myDatas.bio;
+        document.querySelector('#counter-01').innerText = myDatas.reposNum;
+        document.querySelector('#counter-02').innerText = myDatas.followers;
+        document.querySelector('#counter-03').innerText = myDatas.following;
+    }
 }
 
-function getDatas(data) {
-    let myDatas = {
-        name: data.name,
-        bio: data.bio,
-        login: data.login,
-        followers: data.followers,
-        reposNum: data.public_repos,
-        avatar: data.avatar_url,
-        linkProfile: data.html_url,
-        following: data.following
-    };
-    changeInformations(myDatas);
-}
+function getUserRepos() {
+    let endpoint = `https://api.github.com/users/${getUserLogin()}/repos`;
+    fetch(endpoint)
+        .then(response => response.json())
+        .then(data => setInformations(data))
+        .catch(console.error);
 
-function changeInformations(myDatas) {
-    document.querySelector('#profile-avatar').setAttribute('src', myDatas.avatar);
-    document.querySelector('#profile-link').setAttribute('href', myDatas.linkProfile);
-    document.querySelector('#profile-link').innerText = myDatas.name;
-    document.querySelector('#login-name').innerText = myDatas.login;
-    document.querySelector('#bio').innerText = myDatas.bio;
-    document.querySelector('#counter-01').innerText = myDatas.reposNum;
-    document.querySelector('#counter-02').innerText = myDatas.followers;
-    document.querySelector('#counter-03').innerText = myDatas.following;
+    function setInformations(data) {
+        let myElementsUl = [];
+
+        myElementsUl = data.map(function (element) {
+            let liTags = '';
+
+            for (let key in element) {
+                if(element[key] === null) continue;
+
+                if (key === 'name') {
+                    liTags += '<li><h3>' + element[key] + '</h3></li>';
+                } 
+                else if (key === 'html_url') {
+                    liTags += `<li><a href="${element[key]}">Acesse o repositório</a></li>`;
+                }
+                else if (key === 'description' || key === 'language' ||
+                key === 'stargazers_count') {
+                    liTags += '<li>'+ element[key] + '</li>';
+                }
+            }
+
+            return '<ul>' + liTags + '</ul>';
+        })
+
+        document.querySelector('#repos-list').innerHTML = '';
+        document.querySelector('#repos-list').classList.add('generic-section');
+        
+        myElementsUl.forEach(function (ul) {
+            document.querySelector('#repos-list').innerHTML += ul;
+        })
+    }
 }
 
 let searchGithubUser = document.querySelector('#search-github-user');
@@ -60,6 +103,7 @@ closeTab.addEventListener('click', hideAndShowSearch);
 
 let searchAndConfirm = document.querySelector('#confirm');
 searchAndConfirm.addEventListener('click', function () {
-    getUserGithub();
+    getGithubUser();
+    getUserRepos();
     hideAndShowSearch();
 })
